055cca5409e84553c9f24e29aa608a87124fb7d2
Fotios Ioannis Giasemis <fotisfigz@gmail.com>
2022-05-13 14:52:13 +0300

    Automate npm run test

--- 2. A Test-Driven Experience/users/test/reading_test.js
+++ 2. A Test-Driven Experience/users/test/reading_test.js
@@ -10,10 +10,18 @@ describe('Reading users out of the database', () => {
             .then(() => done());
     });
 
-    it('finds all users with name Joe', () => {
+    it('finds all users with name Joe', (done) => {
         User.find({ name: 'Joe' })
             .then((users) => {
                 assert(users[0]._id.toString() === joe._id.toString());
+                done();
+            });
+    });
+
+    it('finds a user with a particular id', (done) => {
+        User.findOne({ _id: joe._id })
+            .then((user) => {
+                assert(user.name === 'Joe');
                 done();
             });
     });
